a9b5d1f8dd8c95acf49596a2885e7f24f3975d81
denysum14 <133210276+denysum14@users.noreply.github.com>
2023-12-03 19:51:50 +0200

    Add files via upload

---
+++ Fibo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Fibonacci
+{
+    static int CalculateFibonacci(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+        else
+        {
+            return CalculateFibonacci(n - 1) + CalculateFibonacci(n - 2);
+        }
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Введіть номер числа Фібоначчі: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n < 0)
+        {
+            Console.WriteLine("Номер числа Фібоначчі має бути не менше 0.");
+        }
+        else
+        {
+            int result = CalculateFibonacci(n);
+            Console.WriteLine($"Число Фібоначчі на позиції {n} дорівнює {result}");
+        }
+    }
+}
